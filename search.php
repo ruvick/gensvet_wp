c0a3996668b4c26efdd6deb8a7ba5b6560e4e2fc
Ruvick <rudikov.web@ya.ru>
2021-08-02 11:32:27 +0300

    Поправил структуру стр Поиск

--- search.php
+++ search.php
@@ -11,8 +11,7 @@ get_header(); ?>
 
 <?php get_template_part('template-parts/header-section'); ?>
 
-<main id="primary" class="main">
-
+<main class="main">
 	<section class="breadcrumb">
 		<div class="container">
 			<?php
@@ -22,30 +21,21 @@ get_header(); ?>
 			?>
 		</div>
 	</section>
-
-	<section class="content">
+	<section class="products">
 		<div class="container">
-
 			<h1>Результаты поиска</h1>
-
 			<div class="products-wrap">
-
 				<?php
 				while (have_posts()) :
 					the_post();
 					get_template_part('template-parts/product-elem');
 				endwhile;
 				?>
-
 			</div>
-
 			<?php if (function_exists('wp_corenavi')) wp_corenavi(); ?>
-
 		</div>
 	</section>
-
 	<?php get_template_part('template-parts/subscription-section'); ?>
-
 </main>
 
 <?php get_footer(); ?>
