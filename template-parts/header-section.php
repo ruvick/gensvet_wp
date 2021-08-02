d4e1b078d9f0f71660eea6e672b6a5d5e50c85f2
Ruvick <rudikov.web@ya.ru>
2021-08-02 12:34:31 +0300

    стрСертификаты - ссылка с главной

--- template-parts/header-section.php
+++ template-parts/header-section.php
@@ -9,7 +9,7 @@
 					<a href="<?php echo get_permalink(6); ?>" class="header-top-nav-item__link">Презентация компании</a>
 				</li>
 				<li class="header-top-nav-item">
-					<a href="<?php echo get_permalink(6); ?>" class="header-top-nav-item__link">Сертификаты и награды</a>
+					<a href="<?php echo get_permalink(149); ?>" class="header-top-nav-item__link">Сертификаты и награды</a>
 				</li>
 				<li class="header-top-nav-item">
 					<a href="<?php echo get_permalink(38); ?>" class="header-top-nav-item__link">Контакты</a>
