bef178248075cfb7de81165be5a553f8a0c67854
Danil Furman <steallermail@gmail.com>
2021-10-13 11:41:24 +0200

    Правка поиска

--- search.php
+++ search.php
@@ -23,6 +23,11 @@ get_header(); ?>
 
 	<section class="products">
 		<div class="container">
+			<form role="search" method="get" id="searchform" action="<?php echo home_url('/') ?>" class="header-bottom-wrap-search page-search-form">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/home/zoom.svg" alt="" class="header-bottom-wrap-search-hidden__img">
+				<input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" class="header-bottom-wrap-search-hidden__input" placeholder="Введите название или артикул товара..." minlength="2" maxlength="40" required>
+				<button type="submit" tabindex="2" id="searchsubmit" value="" class="header-bottom-wrap-search-hidden__btn sub-search">найти</button>
+			</form>
 			<h1>Результаты поиска</h1>
 			<div class="products-wrap">
 				<?php
@@ -31,6 +36,9 @@ get_header(); ?>
 					get_template_part('template-parts/product-elem');
 				endwhile;
 				?>
+				<?php if (!have_posts()) {
+					echo '<p>По указанным данным ничего не найдено, измените параметры поиска.</p>';
+				} ?>
 			</div>
 			<?php if (function_exists('wp_corenavi')) wp_corenavi(); ?>
 		</div>
